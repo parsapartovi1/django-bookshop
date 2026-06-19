69f681ac21c76214474968208e9c95c4f6533e69
parsapartovi <pparsapartovi13@gmail.com>
2026-06-19 21:40:25 +0330

    catalog choices initialized

---
+++ catalog/choices.py
@@ -0,0 +1,57 @@
+KID = "kid"
+MIDDLE_GRADE = "middle_grade"
+TEEN = "teen"
+YOUNG_ADULT = "young_adult"
+ADULT = "adult"
+
+LEVEL_CHOICES = (
+    (KID, "Kid"),
+    (MIDDLE_GRADE, "Middle Grade"),
+    (TEEN, "Teen"),
+    (YOUNG_ADULT, "Young Adult"),
+    (ADULT, "Adult"),
+)
+
+
+ENGLISH = "english"
+SPANISH = "spanish"
+FARSI = "farsi"
+FRENCH = "french"
+
+LANGUAGE_CHOICES = (
+    (ENGLISH, "English"),
+    (SPANISH, "Spanish"),
+    (FARSI, "Farsi"),
+    (FRENCH, "French"),
+)
+
+
+ADVENTURE = "adventure"
+FANTASY = "fantasy"
+SCIENCE = "science"
+MYSTERY = "mystery"
+ROMANCE = "romance"
+HORROR = "horror"
+COMEDY = "comedy"
+HISTORICAL = "historical"
+BIOGRAPHY = "biography"
+SELF_HELP = "self_help"
+EDUCATIONAL = "educational"
+BUSINESS = "business"
+RELIGION = "religion"
+
+GENRE_CHOICES = (
+    (ADVENTURE, "Adventure"),
+    (FANTASY, "Fantasy"),
+    (SCIENCE, "Science"),
+    (MYSTERY, "Mystery"),
+    (ROMANCE, "Romance"),
+    (HORROR, "Horror"),
+    (COMEDY, "Comedy"),
+    (HISTORICAL, "Historical"),
+    (BIOGRAPHY, "Biography"),
+    (SELF_HELP, "Self Help"),
+    (EDUCATIONAL, "Educational"),
+    (BUSINESS, "Business"),
+    (RELIGION, "Religion"),
+)
